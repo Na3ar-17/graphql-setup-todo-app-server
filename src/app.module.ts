617f4e1666eb90144dd47a7d2288599a2e60dbe3
Na3ar-17 <gavruk2017@gmail.com>
2024-08-15 09:05:15 +0200

    configured graphql in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+    }),
+  ],
   controllers: [],
   providers: [PrismaService],
 })
